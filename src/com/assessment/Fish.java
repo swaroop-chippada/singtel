33b42fab0bca12eb2470a224162aa8a994ed9bae
Swaroop.Chippada <Swaroop.Chippada@xchanging.com>
2018-09-27 23:47:16 +0800

    Question B.2
    Added size,color,doesCrackJokes,doesEatOtherFish properties to Fish

--- src/com/assessment/Fish.java
+++ src/com/assessment/Fish.java
@@ -1,9 +1,46 @@
 package com.assessment;
 
 abstract class Fish {
+	
+	private String size;
+	private String color;
+	private boolean doesCrackJokes;
+	private boolean doesEatOtherFish;
 
 	void swim() {
 		System.out.println("i can swim");
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public boolean isDoesCrackJokes() {
+		return doesCrackJokes;
+	}
+
+	public void setDoesCrackJokes(boolean doesCrackJokes) {
+		this.doesCrackJokes = doesCrackJokes;
+	}
+
+	public boolean isDoesEatOtherFish() {
+		return doesEatOtherFish;
+	}
+
+	public void setDoesEatOtherFish(boolean doesEatOtherFish) {
+		this.doesEatOtherFish = doesEatOtherFish;
 	}
 
 }
